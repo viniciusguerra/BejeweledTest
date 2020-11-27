365f2c9f94848cdb970c3d815463b86bed2dd9ac
Vinícius Guerra Cardoso <dev.viniciusguerra@gmail.com>
2020-11-27 05:44:31 -0300

    Change to Windows x64, remove reference to UnityEditor in player script

--- Assets/_Bejeweled/Tiles/TileType.cs
+++ Assets/_Bejeweled/Tiles/TileType.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 // Disable warning that GetHashCode must be overriden along with Equals
