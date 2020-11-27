7cb139d5409728a956d151d7e55d51ffec27ddd7
Vinícius Guerra Cardoso <dev.viniciusguerra@gmail.com>
2020-11-27 03:06:59 -0300

    Add Pooling system

---
+++ Assets/_Bejeweled/Pooling/Pool.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bejeweled
+{
+    [Serializable]
+    public class Pool : MonoBehaviour
+    {
+        private PoolController pool;
+
+        [SerializeField]
+        private string id;
+        public string Id
+        {
+            get => id;
+        }
+
+        [SerializeField]
+        private GameObject prefab;
+
+        [SerializeField]
+        private int initialAmount;
+
+        [Header("Runtime")]        
+        [SerializeField]
+        private Stack<GameObject> instanceStack;
+
+        GameObject _instance;
+
+        private void CreateInstance()
+        {
+            _instance = Instantiate(prefab, transform, false);
+            _instance.SetActive(false);
+
+            instanceStack.Push(_instance);
+        }
+
+        public GameObject RetrieveInstance()
+        {
+            if (instanceStack.Count == 0)
+                CreateInstance();
+
+            _instance = instanceStack.Pop();
+            _instance.SetActive(true);
+            _instance.transform.SetParent(null, false);
+
+            return _instance;
+        }
+
+        public void StoreInstance(GameObject instance)
+        {
+            instance.SetActive(false);
+            instance.transform.SetParent(transform, false);
+            instanceStack.Push(instance);
+        }
+
+        public void Initialize()
+        {
+            instanceStack = new Stack<GameObject>();
+
+            for (int i = 0; i < initialAmount; i++)
+            {
+                CreateInstance();
+            }
+        }
+    }
+}
---
+++ Assets/_Bejeweled/Pooling/PoolController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bejeweled
+{
+    public class PoolController : MonoBehaviour
+    {
+        [SerializeField]
+        private Pool[] poolArray;
+
+        Pool _pool;
+
+        public GameObject RetrieveInstance(string id)
+        {
+            _pool = Array.Find(poolArray, p => p.Id == id);
+
+            return _pool.RetrieveInstance();
+        }
+
+        public void StoreInstance(GameObject instance, string id)
+        {
+            _pool = Array.Find(poolArray, p => p.Id == id);
+
+            _pool.StoreInstance(instance);
+        }
+
+        private void Awake()
+        {
+            foreach (var pool in poolArray)
+            {
+                pool.Initialize();
+            }
+        }
+    }
+}
